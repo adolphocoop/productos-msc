3f7eddbdca1d92502c00ea1e62c0c4c869ed2874
adolpho <adolphovt12@gmail.com>
2023-10-12 22:18:33 -0600

    Cuarto commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,8 +2,12 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
-    withCredentials: true
+    //baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://apimaestria.onrender.com/api',
+    withCredentials: true,
+    headers:{
+        Accept: 'application/json'
+    }
 })
 
 export default instance;
